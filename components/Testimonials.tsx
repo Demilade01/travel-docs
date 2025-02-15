3ff2ebb6ac01ed55d64508e7544d6e2bdba512ea
Demilade01 <demiladeakinyemi2004@gmail.com>
2025-02-14 16:32:24 -0800

    statr faq section

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -4,7 +4,7 @@ import ReviewCard from './ReviewCard'
 
 const Testimonials = () => {
   return (
-    <div className='container mx-auto'>
+    <div className='container mx-auto  mb-[400px]'>
       <div className='flex flex-col items-center justify-center'>
         <p className='uppercase font-bold text-sm bg-[#cfdff3] py-2 px-4 rounded-full'>Raving Reviews</p>
         <h3 className='text-6xl font-extrabold mt-10 sm:text-center w-[720px]'>Hear From Our Satisfied Customers</h3>
@@ -13,7 +13,7 @@ const Testimonials = () => {
 
         <div className='flex justify-between flex-wrap mx-auto items-center w-[650px] h-[400px] gap-4'>
           {customerReviews.map((review) => (
-            <div key={review.name} >
+            <div key={review.name}>
               <ReviewCard
                 review={review.review}
                 name={review.name}
